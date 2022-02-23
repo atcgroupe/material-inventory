d5c357b89417349f7c5c904af50340bfdb8f8042
Pierre Gaimard <pierre@gaimard-family.fr>
2022-02-23 11:50:13 +0100

    add DashboardController

---
+++ src/Controller/Dashboard/DashboardController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller\Dashboard;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractController
+{
+    #[Route('/', name: 'dashboard')]
+    public function index()
+    {
+        return new Response('Hello world !');
+    }
+}
